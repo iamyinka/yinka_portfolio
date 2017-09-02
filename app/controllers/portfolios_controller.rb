b21ca3fbda31dce54cdf6e17f1ae8713cde6bf4e
Yinka Ash <developer@designsbynetguru.com>
2017-09-02 10:20:32 +0800

    Add new & create actions to portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,8 +4,24 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.all
 	end
 
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+
+		if @portfolio_item.save
+			redirect portfolios_path
+		else
+			render 'new'
+		end
+	end
 
 
+	private
 
-
+	def portfolio_params
+		params.require(:portfolio).permit(:title, :subtitle, :body)
+	end
 end
